0dc8995fd9357fcb4d0ffa3e9199af755dc98668
Shist <shist.pupust@mail.ru>
2023-12-19 05:43:15 +0300

    HW 2, try 1 : Stores list from js file is now loading to the page

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,38 @@
 "use strict";
 
+import { Stores } from "../data/data.js";
+
 document.addEventListener("DOMContentLoaded", () => {
-  // Start logic here . . .
+  const noStoresLayout = document.querySelector(
+    ".stores-list-section__no-data-layout"
+  );
+
+  if (Stores.length) {
+    noStoresLayout.classList.add("js-hidden-element");
+  }
+
+  let storesListToAddStr = "";
+
+  Stores.forEach((store) => {
+    storesListToAddStr += `
+        <div class="stores-list-item">
+            <div class="stores-list-item__name-address-wrapper">
+                <h3 class="stores-list-item__name-headline">
+                    ${store.Name}
+                </h3>
+                <span class="stores-list-item__address-text"
+                    >${store.Address}</span
+                >
+            </div>
+            <div class="stores-list-item__area-data-wrapper">
+                <span class="stores-list-item__area-number">${store.FloorArea}</span>
+                <span class="stores-list-item__area-unit">sq.m</span>
+            </div>
+        </div>
+        `;
+  });
+
+  const storesListSection = document.querySelector(".stores-list-section");
+
+  storesListSection.insertAdjacentHTML("afterbegin", storesListToAddStr);
 });
