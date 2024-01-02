e88943b00030d8de231c8c0588dc3f6c0c86c884
Shist <shist.pupust@mail.ru>
2024-01-02 15:14:58 +0300

    HW 4, try 1 : Prepared static constants for controller

--- scripts/controller.js
+++ scripts/controller.js
@@ -1,6 +1,22 @@
 "use strict";
 
 class Controller {
+  static LOCAL_STORAGE_ID = {
+    CURR_STORE_ID: "currStoreId",
+    BOOKMARK_DETECTED: "bookmarkIsDetected",
+    CURR_PRODUCT_ID: "currProductId",
+    CURR_FILTER_ID: "currFilterId",
+    CURR_SORT_KEY: "currSortKey",
+    CURR_SORT_ORDER: "currSortOrder",
+    STORES_LIST_SPINNER_FETHES: "storesListSpinnerFetches",
+    DETAILS_SPINNER_FETHES: "detailsSpinnerFetches",
+    FILTERS_SPINNER_FETHES: "filtersSpinnerFetches",
+    PRODUCTS_LIST_SPINNER_FETHES: "productsListSpinnerFetches",
+    EDIT_PRODUCT_FORM_SPINNER_FETHES: "editProductFormSpinnerFetches",
+  };
+
+  static BOOKMARK_QUERY_STORE_ID = "store-id";
+
   constructor(view, model) {
     this.view = view;
     this.model = model;
